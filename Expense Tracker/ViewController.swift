449d50a45d526340806b2d3f8f64f8eee3edb36f
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-01-20 09:59:58 +0500

    Initial Commit

---
+++ Expense Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense Tracker
+//
+//  Created by Ravshanbek Tursunbaev on 2023/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
